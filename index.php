fe90105979ad9bf6ca1d2f5e349ee8c2179de4b1
Mohtadi Bakali <teddybak@hotmail.com>
2020-08-17 12:50:04 +0100

    initial commit

--- index.php
+++ index.php
@@ -105,18 +105,18 @@
                       About Porchester Interiors
                     </h5>
                   </div>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id
                     imperdiet et, porttitor
                     at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla
                     porttitor accumsan tincidunt.
                   </p>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus suscipit tortor eget felis
                     porttitor volutpat. Vestibulum
                     ac diam sit amet quam vehicula elementum sed sit amet dui. porttitor at sem.
                   </p>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
                     Nulla porttitor accumsan
                     tincidunt. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
@@ -128,18 +128,18 @@
                   <div class="title-box-2">
                     <br>
                   </div>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id
                     imperdiet et, porttitor
                     at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla
                     porttitor accumsan tincidunt.
                   </p>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vivamus suscipit tortor eget felis
                     porttitor volutpat. Vestibulum
                     ac diam sit amet quam vehicula elementum sed sit amet dui. porttitor at sem.
                   </p>
-                  <p class="lead">
+                  <p class="lead text-justify">
                     Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim.
                     Nulla porttitor accumsan
                     tincidunt. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
