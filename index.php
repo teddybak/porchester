af83dc879444f641fa937be3d3caf5e65294cbe7
Mohtadi Bakali <teddybak@hotmail.com>
2020-08-30 18:06:11 +0100

    in progress

--- index.php
+++ index.php
@@ -1,100 +1,43 @@
-<div id="fullwidth-video">
-  <div class="fullwidth-video-bg">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="estilo.css" />
+
+  <title>Document</title>
+</head>
+<body>
+<div class="vid-container">
+  <video
+          class="bgvid"
+          autoplay="autoplay"
+          muted="muted"
+          preload="auto"
+          loop
+  >
+    <source src="assets/videos/video.mp4" type="video/mp4" />
+  </video>
+  <div class="inner-container">
     <video
-      playsinline
-      autoplay
-      muted
-      onplaying="this.controls=false"
-      loop
-      id="myvid"
+            class="bgvid inner"
+            autoplay="autoplay"
+            muted="muted"
+            preload="auto"
+            loop
     >
-      <source
-        src="assets/videos/video.mp4"
-        type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
-      />
+      <source src="assets/videos/video.mp4" type="video/mp4" />
     </video>
+    <div class="box">
+      <h1>Porchester Interiors</h1>
+      <button>Skip Intro</button>
+    </div>
   </div>
 </div>
-<div id="porchester">
-  <h1 class="display-3">Porchester Interiors</h1>
-  <h3 c>"First hand experience"</h3>
-  <a href="home.html" class="close">Skip intro</a>
-</div>
-
+</body>
 <script>
-  video = document.getElementById("myvid");
-  video.addEventListener("ended", function () {
-    alert("video is ended");
-    window.location.href = "home.html";
-  });
+  document.querySelector("body").onclick = function () {
+    location.href = "home.html";
+  };
 </script>
-
-<link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
-<style>
-  #porchester {
-    position: fixed;
-    top: 3%;
-    margin-left: 5%;
-    z-index: 5000;
-    color: white;
-  }
-  #fullwidth-video {
-    max-width: 100%;
-    min-height: 100%;
-    position: relative;
-  }
-
-  #fullwidth-video .fullwidth-video-bg {
-    position: absolute;
-    z-index: 1;
-    top: 0px;
-    left: 0px;
-    bottom: 0px;
-    right: 0px;
-    overflow: hidden;
-    background-size: cover;
-    background-color: transparent;
-    background-repeat: no-repeat;
-    background-position: 0% 50%;
-    background-image: url(http://mysite.com/assets/images/bg/myvideobg.jpg);
-  }
-
-  #fullwidth-video video {
-    margin: auto;
-    position: absolute;
-    z-index: 1;
-    top: 50%;
-    left: 0%;
-    transform: translate(0%, -50%);
-    visibility: visible;
-    opacity: 1;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  /** For Edge**/
-  @supports (-ms-ime-align: auto) {
-    #fullwidth-video video {
-      object-fit: none;
-      margin: none;
-      position: inherit;
-      z-index: 1;
-      top: 50%;
-      left: 0%;
-      transform: translate(0%, -50%);
-      height: auto;
-      width: 100%;
-    }
-    h3 {
-      font-style: italic;
-    }
-  }
-  .close {
-    position: fixed;
-    color: white !important;
-    z-index: 5000;
-  }
-</style>
+</html>
