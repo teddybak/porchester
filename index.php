f34d134a741548e8beb62fb0fb6034f61a77583f
tedybak <tedybak@hotmail.com>
2020-08-31 11:14:41 +0100

    in progress

--- index.php
+++ index.php
@@ -1,78 +1,25 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>
-<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>
-<html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js"> <!--<![endif]-->
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Jquery Youtube Fullscreen Background Video</title>
-    <meta name="keywords" content="background, video, jquery, plugin, javascript, css, 360, youtube">
-    <meta name="description" content="Create fullscreen youtube background videos with this jquery plugin.">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Twitter Boostrap for Demo -->
-     <!-- styles for video player -->
- </head>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
 <body>
+<div id="video_wrapper">
 
-<div id="background-video" class="background-video">
+    <video autoplay muted loop>
+        <source src="video/intro.mp4" type="video/mp4">
+    </video>
+
+    <div id="wrapper">
+        <h1>Porchester Interiors</h1>
+        <span class="experience">'First Hand Experience'</span>
+    </div>
+
 </div>
-
-
-<script src="lib/jquery/jquery.min.js"></script>
-<!-- ADD Jquery Video Background -->
-<script src="src/jquery.youtubebackground.js"></script>
-<script>
-    jQuery(function ($) {
-
-        $('#module-video').YTPlayer({
-            fitToBackground: false,
-            videoId: 'jKCyFB5LmPo',
-            pauseOnScroll: false,
-            playerVars: {
-                modestbranding: 0,
-                autoplay: 1,
-                controls: 1,
-                showinfo: 0,
-                branding: 0,
-                rel: 0,
-                autohide: 0
-            }
-        });
-
-
-        $('#background-video').YTPlayer({
-            fitToBackground: false,
-            videoId: 'Y9yRL24gWhk',
-            pauseOnScroll: true,
-            callback: function () {
-                videoCallbackEvents();
-            }
-        });
-
-        var videoCallbackEvents = function () {
-            var player = $('#background-video').data('ytPlayer').player;
-
-            player.addEventListener('onStateChange', function (event) {
-                console.log("Player State Change", event);
-
-                // OnStateChange Data
-                if (event.data === 0) {
-                    console.log('video ended');
-                } else if (event.data === 2) {
-                  window.location.href='home.php';
-                }
-            });
-        }
-
-
-    });
-
-
-</script>
 </body>
+<script>
+var body = document.querySelector("body");
+body.addEventListener('click', function (){
+document.location.href = '<?php echo "home.php"; ?>';
+})
+</script>
 </html>
